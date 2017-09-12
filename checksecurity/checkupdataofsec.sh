baa271ecc0d16b63ec99fdef1e2fa76368465b30
sccxboy <sccxboy@gmail.com>
2017-09-13 03:08:26 +0800

    Modify check OS/software security update.

--- checksecurity/checkupdataofsec.sh
+++ checksecurity/checkupdataofsec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Start check updata of security..............."
-apt-get upgrade -s | grep -i security > /tmp/updatasec
+apt-get upgrade -s | grep -i security  | grep Inst > /tmp/updatasec
 echo "Updata of security info :"
 linec=`cat /tmp/updatasec | wc -l`
 if [ $linec -gt 0 ]; then
